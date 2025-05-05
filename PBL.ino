33d1f26571dab8627373a26d417d696c01fe2c2d
LeeMendez-Cartmill <lee.mendez-cartmill@dawsoncollege.qc.ca>
2025-05-05 09:23:32 -0400

    Update PBL.ino

--- PBL.ino
+++ PBL.ino
@@ -1,6 +1,6 @@
 #include <RBD_WaterSensor.h> // water senseor library
-#include <RBD_Capacitance.h> // required to properly run the RBD_WaterSensor
-#include <RBD_Threshold.h> // required to properly run the RBD_WaterSensor
+#include <RBD_Capacitance.h> // Dependecy required to properly run the RBD_WaterSensor
+#include <RBD_Threshold.h> // Dependecy required to properly run the RBD_WaterSensor
 #define WATER_SENSOR_PIN A0 
 void Water_Level(){
   int H2OValue = analogRead(A0);

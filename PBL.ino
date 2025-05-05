d6d25f9cc57836b47f24ee797f8c849f41457539
LeeMendez-Cartmill <lee.mendez-cartmill@dawsoncollege.qc.ca>
2025-05-05 09:11:51 -0400

    Add files via upload

---
+++ PBL.ino
@@ -0,0 +1,18 @@
+#include <RBD_WaterSensor.h> // water senseor library
+#include <RBD_Capacitance.h> // required to properly run the RBD_WaterSensor
+#include <RBD_Threshold.h> // required to properly run the RBD_WaterSensor
+#define WATER_SENSOR_PIN A0 
+void Water_Level(){
+  int H2OValue = analogRead(A0);
+  int outputValue = (H2OValue)/1023;
+}
+void setup() {
+  //begin the serial print
+ Serial.begin(9600);
+ pinMode(WATER_SENSOR_PIN, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
